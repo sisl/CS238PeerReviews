83f8677ebc5b8bf366131fea24a72de87ccc9944
Jenny <jy2k16@gmail.com>
2023-12-19 15:44:12 -0800

    changes for returning final projects

--- parse_data.py
+++ parse_data.py
@@ -9,10 +9,10 @@ import sys
 import yaml
 from PyPDF2 import PdfWriter, PdfReader
 sys.setrecursionlimit(300000)
-random.seed(228)
+random.seed(227)
 
-roster_filename = "Course_Roster.csv"
-projects_filename = "Gradescope_Final_Assignment.csv"
+roster_filename = "updated_AA228_Fall_2023_roster.csv" #TODO: remember to change
+projects_filename = "/Users/jennyyang/Downloads/organized_submissions/real_updated_csvfile.csv"
 
 class Student(object):
     def __init__(self,first_name,last_name,sid,email,section) -> None:
@@ -90,13 +90,19 @@ def find_student_by_email(email,student_list):
     if pd.isna(email):
         return None
 
+    # print('all emails',email)
+    email = email.rstrip()
+
     for s in student_list:
+        # print(s.email)
         if s.email==email:
             student = s
             return student
 
     if student is None:
-        raise NotImplementedError
+        # TODO: FIGURE OUT WHAT'S GOING ON HERE.
+        print("email", email)
+        # raise NotImplementedError
 
 # def find_student_by_sid(sid,student_list):
 #     student = None
@@ -113,31 +119,52 @@ def find_student_by_email(email,student_list):
 
 def get_projects_list(students_list):
     projects_list = []
+    seen_projects = set()
+    # TODO: REMOVE SEEN PROJECTS?
     df = pd.read_csv(projects_filename)
+    df.rename(columns={'Question 1 Response': 'project_title'}, inplace=True)
+    df.rename(columns={'Question 3 Response': 'publish'}, inplace=True)
+    df.rename(columns={'Question 4 Response': 'pr_exemption'}, inplace=True)
+    df.rename(columns={'Question 2.1 Response': 'auth_1_email'}, inplace=True)
+    df.rename(columns={'Question 2.2 Response': 'auth_2_email'}, inplace=True)
+    df.rename(columns={'Question 2.3 Response': 'auth_3_email'}, inplace=True)
+    df.rename(columns={'Question 2.4 Response': 'auth_4_email'}, inplace=True)
+
     for i in range(len(df)):
+        if pd.isna(df.iloc[i]["project_id"]):
+            continue
+
         project_id = str(int(df.iloc[i]["project_id"])).zfill(3)
+        if project_id in seen_projects:
+            continue
         project_title = df.iloc[i]["project_title"]
-        permission_publish = df.iloc[i]["publish"]
-        pr_exemption = df.iloc[i]["pr_exemption"]
+        if df.iloc[i]["publish"] == "Yes":
+            permission_publish = True
+        else:
+            permission_publish = False
+        if df.iloc[i]["pr_exemption"] == "Yes":
+            pr_exemption = True
+        else:
+            pr_exemption = False
 
         #find_author 1
         auth_1_email = df.iloc[i]["auth_1_email"]
-        s1 = find_student_by_email(auth_1_email,student_list)
+        s1 = find_student_by_email(auth_1_email,students_list)
 
         #find_author 2
         auth_2_email = df.iloc[i]["auth_2_email"]
-        s2 = find_student_by_email(auth_2_email,student_list)
+        s2 = find_student_by_email(auth_2_email,students_list)
 
         #find_author 3
         auth_3_email = df.iloc[i]["auth_3_email"]
-        s3 = find_student_by_email(auth_3_email,student_list)
+        s3 = find_student_by_email(auth_3_email,students_list)
 
         #find_author 4
         auth_4_email = df.iloc[i]["auth_4_email"]
-        s4 = find_student_by_email(auth_4_email,student_list)
+        s4 = find_student_by_email(auth_4_email,students_list)
 
         #find file
-        res = glob("./project_files/"+project_id+"*")
+        res = glob("/Users/jennyyang/Downloads/organized_submissions/"+project_id+"*")
         
         # if project_id=="136":
         #     filename = None
@@ -146,7 +173,7 @@ def get_projects_list(students_list):
         else:
             filename = res[0].split("/")[-1]
         
-        
+        seen_projects.add(project_id)
         projects_list.append(Project(project_id,filename,project_title,s1,s2,s3,s4,permission_publish,pr_exemption))
 
     return projects_list
@@ -175,18 +202,20 @@ def assign_peer_reviews(student_list,projects_list):
     - not same assignemnt twice
     - not own project
     """
+    print("pre filter length of projects", len(projects_list))
     #ONLY CONSIDER PROJECTS THAT ARE OK WITH PEER REVIEW
     filtered_projects_list = []
     for p in projects_list:
         if not p.pr_exemption:
             filtered_projects_list.append(p)
-    projects_list = filtered_projects_list
+    projects_list = list(set(filtered_projects_list))
     # if len(projects_list) <= 3:
     #     raise NotImplementedError
     
-    
+    print("length of projects", len(projects_list))
     current_projects_list = copy(projects_list)
     next_projects_list = copy(projects_list)
+    # last_projects_list = copy(projects_list)
 
     for s in student_list:
         # print(len(current_projects_list))
@@ -218,6 +247,21 @@ def assign_peer_reviews(student_list,projects_list):
                     project = next_projects_list.pop(0)
                     s.peer_review_1 = project
                     assigned = True
+        
+        # elif not np.any([next_projects_list[0].auth_1 == s, next_projects_list[0].auth_2 == s, next_projects_list[0].auth_3 == s, next_projects_list[0].auth_4 == s]):
+        #     project = next_projects_list.pop(0)
+        #     s.peer_review_1 = project
+        #     assigned = True
+        
+        # else:
+        #     assigned = False
+        #     while not assigned:
+        #         random.shuffle(last_projects_list)
+
+        #         if not np.any([last_projects_list[0].auth_1 == s, last_projects_list[0].auth_2 == s, last_projects_list[0].auth_3 == s, last_projects_list[0].auth_4 == s]):
+        #             project = last_projects_list.pop(0)
+        #             s.peer_review_1 = project
+        #             assigned = True
         
         if len(current_projects_list) == 0:
             current_projects_list = next_projects_list
@@ -272,7 +316,11 @@ def helper_check_not_self_assigned(student_list, projects_list):
             if np.any([p.auth_1 == s, p.auth_2 == s, p.auth_3 == s, p.auth_4 == s]):
                 s_project = p.project_id
         
-        print(f"{s.sid} {s_project} {s_peer_review_1} {s_peer_review_2}")
+        if s_project == s_peer_review_1 or s_project == s_peer_review_2:
+            print(f'{s.id} is reviewing their own project. {s_project} {s_peer_review_1} {s_peer_review_2}')
+        if s_peer_review_1 == s_peer_review_2:
+            print(f'{s.sid} is reviewing the same project twice...')
+        # print(f"{s.sid} {s_project} {s_peer_review_1} {s_peer_review_2}")
 
 def write_peer_review_assignments(student_list):
     sids = [s.sid for s in student_list]
@@ -308,12 +356,12 @@ def write_peer_review_projects(pr1_list,pr2_list,projects_list):
         peer_review_files = []
         for pr in pr1_list:
             if pr.project == p:
-                peer_review_files.append("./peer_review_1_files/"+pr.filename)
+                peer_review_files.append("/Users/jennyyang/Downloads/assignment_3790759_export_PR1/"+pr.filename)
                 peer_review_list.append(pr)
 
         for pr in pr2_list:
             if pr.project == p:
-                peer_review_files.append("./peer_review_2_files/"+pr.filename)
+                peer_review_files.append("/Users/jennyyang/Downloads/assignment_3790761_export_PR2/"+pr.filename)
                 peer_review_list.append(pr)
         
         #write pdf
@@ -337,12 +385,12 @@ def write_permission_to_publish(projects_list):
     project_titles = []
     project_ids = []
     project_filenames = []
-    for p in projects_list:
+    project_set = set(projects_list)
+    for p in project_set:
         if p.permission_to_publish:
             
-            
             #copy files
-            f = glob("./project_files/"+p.project_id+"*")
+            f = glob("/Users/jennyyang/Downloads/organized_submissions/"+p.project_id+"*")
             if len(f) == 0:
                 #cannot publish if not part of peer review
                 continue
@@ -355,7 +403,7 @@ def write_permission_to_publish(projects_list):
                 shutil.copy(f,"./publishable_projects/"+f_name)
             else:
                 raise NotImplementedError
-    dict = {"Title":project_titles, 
+    dict = {"Title":project_titles,
             "Filenames": project_filenames}
     
     df = pd.DataFrame(dict)
@@ -400,27 +448,58 @@ def load_assignments(filename):
     return dict["student_list"], dict["projects_list"]           
 
 def check_equal_dist(student_list,projects_list):
-    for p in projects_list:
+    for p in set(projects_list):
         counter = 0
         for s in student_list:
             if s.peer_review_1 == p:
                 counter += 1
             if s.peer_review_2 == p:
                 counter += 1
+
         # print(counter)
-        if counter<=3:
-            print(p.project_id)
+        if counter == 0:
+            print("zero", p.project_id)
+        if counter == 1:
+            print("one", p.project_id)
+        if counter >= 4:
+            print(">=4", p.project_id, counter)
+        # if counter<=3:
+        #     print(p.project_id)
+
+
+student_list = []
+projects_list = []
+def run_first():
+    student_list = get_student_list()
+    projects_list = get_projects_list(student_list)
+    assign_peer_reviews(student_list,projects_list)
+    save_assignments(student_list,projects_list,"master_assignments2.pkl")
+    write_peer_review_assignments(student_list)
+
+    write_permission_to_publish(projects_list)
+    print("stop")
+
+# run_first()
+
+
+def run_second():
+    student_list, projects_list = load_assignments("master_assignments2.pkl")
+    for p in projects_list:
+        print(p.auth_1)
+    print(len(projects_list))
+
+    write_master_peer_review_assignments(student_list)
+    # check_equal_dist(student_list, projects_list)
+
+    helper_check_not_self_assigned(student_list, projects_list)
+
+    write_permission_to_publish(projects_list)
+    print("stop")
+
+# run_second()
 
 
 
-# student_list = get_student_list()
-# projects_list = get_projects_list(student_list)
-# assign_peer_reviews(student_list,projects_list)
-# save_assignments(student_list,projects_list,"master_assignments2.pkl")
-# write_peer_review_assignments(student_list)
-student_list, projects_list = load_assignments("master_assignments.pkl")
-# write_master_peer_review_assignments(student_list)
-# check_equal_dist(student_list, projects_list)
 
 # ######WRONG UPLOAD ORDER SWAP PROCEDURE#########
 # student_idx = 353
@@ -448,15 +527,21 @@ student_list, projects_list = load_assignments("master_assignments.pkl")
 # ##################################################
 
 
-# with open("peer_review_1_metadata.yml","r") as f:
-#     pr1_metadata = yaml.safe_load(f)
-# pr1_list = get_peer_review_list(student_list,projects_list,pr1_metadata,1)
+def run_third():
+    student_list, projects_list = load_assignments("master_assignments2.pkl")
+    with open("submission_metadata_PR1.yml","r") as f:
+        pr1_metadata = yaml.safe_load(f)
+    pr1_list = get_peer_review_list(student_list,projects_list,pr1_metadata,1)
 
-# with open("peer_review_2_metadata.yml","r") as f:
-#     pr2_metadata = yaml.safe_load(f)
-# pr2_list = get_peer_review_list(student_list,projects_list,pr2_metadata,2)
-# write_peer_review_projects(pr1_list, pr2_list, projects_list)
+    with open("submission_metadata_PR2.yml","r") as f:
+        pr2_metadata = yaml.safe_load(f)
+    pr2_list = get_peer_review_list(student_list,projects_list,pr2_metadata,2)
+    write_peer_review_projects(pr1_list, pr2_list, projects_list)
 
-write_permission_to_publish(projects_list)
-print("stop")
+run_third()
+
+# write_permission_to_publish(projects_list)
+# print("stop")
+
+
 # helper_check_not_self_assigned(student_list, projects_list)
